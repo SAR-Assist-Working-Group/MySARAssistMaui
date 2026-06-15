526e86845c186f9545e550293a6cc3d72c27d749
Gustavo Nobrega <gustavofn@gmail.com>
2026-06-15 09:56:08 -0700

    Add Urgency Assessment button to Home page

--- MySARAssist/MainPage.xaml.cs
+++ MySARAssist/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using MySARAssist.Services;
 using MySARAssist.Views;
 using MySARAssist.Views.CheckInOut;
+using MySARAssist.Views.UrgencyViews;
 using MySarAssistModels.People;
 
 namespace MySARAssist
@@ -57,6 +58,12 @@ namespace MySARAssist
 
 
 
+        }
+
+        private async void UrgencyButton_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync(nameof(UrgencyCalculatorView));
+            _logger.Log(Microsoft.Extensions.Logging.LogLevel.Information, "Moving to the urgency assessment page");
         }
     }
 
