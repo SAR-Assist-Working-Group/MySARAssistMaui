17aa7ddbc55a4078b21346f3f18d7cd773679dd3
DC Baker <dylan@greathat.ca>
2026-08-29 17:17:11 -0700

    Fixed an error from sentry when people tap too fast

--- MySARAssist/Views/CheckInOutViews/CheckInOutView.xaml.cs
+++ MySARAssist/Views/CheckInOutViews/CheckInOutView.xaml.cs
@@ -7,66 +7,101 @@ namespace MySARAssist.Views.CheckInOut;
 
 public partial class CheckInOutView : ContentPage
 {
-    private readonly ILogger<MainPage> logger;
+	private readonly ILogger<MainPage> logger;
+	private bool _isNavigating = false;
 
-    public CheckInOutView(ILogger<MainPage> logger)
+	public CheckInOutView(ILogger<MainPage> logger)
 	{
-        InitializeComponent();
-        this.logger = logger;
-    }
+		InitializeComponent();
+		this.logger = logger;
+	}
 
-    protected override void OnAppearing()
-    {
-        base.OnAppearing();
-        LogController.ResumeShakeIfNeeded();
-        if(this.BindingContext != null)
-        {
-            ViewModels.CheckInOut.CheckInManagementViewModel vm = (ViewModels.CheckInOut.CheckInManagementViewModel)this.BindingContext;
-            vm.OnAppearing();
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+		LogController.ResumeShakeIfNeeded();
+		if(this.BindingContext != null)
+		{
+			ViewModels.CheckInOut.CheckInManagementViewModel vm = (ViewModels.CheckInOut.CheckInManagementViewModel)this.BindingContext;
+			vm.OnAppearing();
 
-        }
-    }
+		}
+	}
 
-    private async void btnAddUser_Clicked(object sender, EventArgs e)
-    {
-        //await Navigation.PushAsync(new Views.CheckInOut.PersonnelEditView());
-        await Shell.Current.GoToAsync("" + nameof(CheckInOutView) + "/" + nameof(PersonnelEditView));
+	private async void btnAddUser_Clicked(object sender, EventArgs e)
+	{
+		if (_isNavigating) { return; }
+		try
+		{
+			_isNavigating = true;
+			//await Navigation.PushAsync(new Views.CheckInOut.PersonnelEditView());
+			await Shell.Current.GoToAsync("" + nameof(CheckInOutView) + "/" + nameof(PersonnelEditView));
+		}
+		finally
+		{
+			_isNavigating = false;
+		}
+	}
 
-    }
+	private async void btnSignOut_Clicked(object sender, EventArgs e)
+	{
 
-    private async void btnSignOut_Clicked(object sender, EventArgs e)
-    {
+	}
 
-    }
+	private async void btnSignIn_Clicked(object sender, EventArgs e)
+	{
 
-    private async void btnSignIn_Clicked(object sender, EventArgs e)
-    {
+	}
 
-    }
+	private async  void btnChangeSelectedMember_Clicked(object sender, EventArgs e)
+	{
+		if (_isNavigating) { return; }
+		try
+		{
+			_isNavigating = true;
+			await Shell.Current.GoToAsync("" + nameof(CheckInOutView) + "/" + nameof(PersonnelListView));
+		}
+		finally
+		{
+			_isNavigating = false;
+		}
+	}
 
-    private async  void btnChangeSelectedMember_Clicked(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync("" + nameof(CheckInOutView) + "/" + nameof(PersonnelListView));
-    }
+	private async void btnEditMember_Clicked(object sender, EventArgs e)
+	{
+		if (_isNavigating) { return; }
+		try
+		{
+			_isNavigating = true;
+			//await Navigation.PushAsync(new Views.CheckInOut.PersonnelEditView());
+			if (App.CurrentPerson == null)
+			{
+				await Shell.Current.GoToAsync("" + nameof(CheckInOutView) + "/" + nameof(PersonnelEditView));
+			}
+			else
+			{
+				await Shell.Current.GoToAsync("" + nameof(CheckInOutView) + "/" + nameof(PersonnelEditView) + $"PersonnelID={App.CurrentPerson.ID.ToString()}");
 
-    private async void btnEditMember_Clicked(object sender, EventArgs e)
-    {
-        //await Navigation.PushAsync(new Views.CheckInOut.PersonnelEditView());
-        if (App.CurrentPerson == null)
-        {
-            await Shell.Current.GoToAsync("" + nameof(CheckInOutView) + "/" + nameof(PersonnelEditView));
-        }
-        else
-        {
-            await Shell.Current.GoToAsync("" + nameof(CheckInOutView) + "/" + nameof(PersonnelEditView) + $"PersonnelID={App.CurrentPerson.ID.ToString()}");
+				//await Shell.Current.GoToAsync($"CheckInOut/EditPersonnel?PersonnelID={App.CurrentPerson.ID.ToString()}");
+			}
+		}
+		finally
+		{
+			_isNavigating = false;
+		}
+	}
 
-            //await Shell.Current.GoToAsync($"CheckInOut/EditPersonnel?PersonnelID={App.CurrentPerson.ID.ToString()}");
-        }
-    }
-
-    private async void btnSelectUser_Clicked(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync("" + nameof(CheckInOutView) + "/" + nameof(PersonnelListView));
-
-    }
+	private async void btnSelectUser_Clicked(object sender, EventArgs e)
+	{
+		if (_isNavigating) { return; }
+		try
+		{
+			_isNavigating = true;
+			await Shell.Current.GoToAsync("" + nameof(CheckInOutView) + "/" + nameof(PersonnelListView));
+		}
+		finally
+		{
+			_isNavigating = false;
+		}
+	}
 }
